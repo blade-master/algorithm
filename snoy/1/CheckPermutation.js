6047a6ff74500ebf0cca5c79db50ec99db8d97c8
snoy <jxycbjhc@163.com>
2019-04-09 20:26:07 +0800

    缺少测试用例

---
+++ snoy/1/CheckPermutation.js
@@ -0,0 +1,16 @@
+"use strict";
+// 把字符串先转成数组，然后排序，最后合并成字符串进行比较
+
+/**
+ * 判断两个字符串是否是同一个字符串的全排列组合
+ * @param {*} s1 
+ * @param {*} s2 
+ */
+function permutation(s1, s2) {
+  return  Array.from(s1).sort().join('')===Array.from(s2).sort().join('');
+}
+
+module.exports = {
+  permutation: permutation
+};
+console.log(permutation('s1o20','s0o21'));
---
+++ structure/ListNode.js
@@ -0,0 +1,25 @@
+// Definition for single-linked lists .
+
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+  }
+  
+  ListNode.generateList = function (num) {
+    var numbers = num.toString().split('').map(Number);
+    var result;
+    var tmp;
+    numbers.forEach(function (number, index) {
+      if (index === 0) {
+        result = new ListNode(number);
+        return;
+      }
+      tmp = new ListNode(number);
+      tmp.next = result;
+      result = tmp;
+    });
+    return result;
+  };
+  
+  module.exports = ListNode;
+  
